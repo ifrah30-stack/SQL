49d60ce413d0ec1aa5bd74961a437f41ab3d8ef8
ifrah30-stack <Ifrahanjum3004@gmail.com>
2025-08-21 19:43:18 +0530

    Create JOIN between multiple tables.sql

---
+++ JOIN between multiple tables.sql
@@ -0,0 +1,5 @@
+SELECT o.order_id, o.order_date, c.customer_name, p.product_name
+FROM orders o
+JOIN customers c ON o.customer_id = c.customer_id
+JOIN order_items oi ON o.order_id = oi.order_id
+JOIN products p ON oi.product_id = p.product_id;
