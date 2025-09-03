99c67e3abeea458759a1c3a674da9dd4ff9dfcad
ifrah30-stack <Ifrahanjum3004@gmail.com>
2025-09-03 19:17:05 +0530

    Create Database Setup.sql

---
+++ Database Setup.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Students (
+    student_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    department VARCHAR(50),
+    marks INT
+);
+
+CREATE TABLE Departments (
+    dept_id INT PRIMARY KEY,
+    dept_name VARCHAR(50)
+);
